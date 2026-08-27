9613448712acdbc3afc696ba260d0070ffbffc01
ahzaab <ahzaab@outlook.com>
2026-08-26 20:42:09 -0400

    Persist debug loading diagnostics

--- src/main.cpp
+++ src/main.cpp
@@ -22,37 +22,28 @@ namespace
         return true;
     }
 
-    bool AttachDebugConsoleSink()
-    {
-        auto log = spdlog::default_logger();
-        if (!log) {
-            return false;
-        }
-        log->sinks().push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-        log->set_level(spdlog::level::trace);
-        log->flush_on(spdlog::level::trace);
-        return true;
-    }
 #endif
 
     bool InitializeLog()
     {
+        auto path = logger::log_directory();
+        if (!path) {
+            return false;
+        }
+        *path /= "SkyrimLoadProgress.log";
+        auto file = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
 #ifndef NDEBUG
         if (!OpenDebugConsole()) {
             return false;
         }
         auto msvc = std::make_shared<spdlog::sinks::msvc_sink_mt>();
         auto console = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        auto log = std::make_shared<spdlog::logger>("multi_sink", spdlog::sinks_init_list{ msvc, console });
+        auto log = std::make_shared<spdlog::logger>(
+            "global", spdlog::sinks_init_list{ file, msvc, console });
         log->set_level(spdlog::level::trace);
+        log->flush_on(spdlog::level::trace);
 #else
-        auto path = logger::log_directory();
-        if (!path) {
-            return false;
-        }
-        *path /= "SkyrimLoadProgress.log";
-        auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-        auto log = std::make_shared<spdlog::logger>("global log", std::move(sink));
+        auto log = std::make_shared<spdlog::logger>("global", std::move(file));
         log->set_level(spdlog::level::info);
         log->flush_on(spdlog::level::info);
 #endif
@@ -71,15 +62,10 @@ namespace
 SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
     try {
+        SKSE::Init(a_skse);
         if (!InitializeLog()) {
             return false;
         }
-        SKSE::Init(a_skse);
-#ifndef NDEBUG
-        if (!AttachDebugConsoleSink()) {
-            return false;
-        }
-#endif
         logger::info("Skyrim Load Progress {} loading", Version::NAME);
         if (!SKSE::GetMessagingInterface()->RegisterListener("SKSE", MessageHandler)) {
             logger::critical("could not register SKSE message listener");
